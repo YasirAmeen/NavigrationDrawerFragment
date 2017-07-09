bb8a6c3ddd8499c289381f2599a24a5f7dd7f900
Yasir Ameen <YasirAmeen92@gmail.com>
2017-07-09 13:51:59 +0500

    intial Commit

---
+++ app/src/main/java/eureka/com/navigationdrawerdemo/HomeFragment.java
@@ -0,0 +1,35 @@
+package eureka.com.navigationdrawerdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+
+import com.labo.kaji.fragmentanimations.CubeAnimation;
+import com.labo.kaji.fragmentanimations.PushPullAnimation;
+
+/**
+ * Created by hp on 7/9/2017.
+ */
+
+public class HomeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.home_fragment,container,false);
+
+        return view;
+    }
+
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+
+        return PushPullAnimation.create(CubeAnimation.LEFT,enter,500);
+    }
+}
